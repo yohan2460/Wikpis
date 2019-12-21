338c91c787e5475959b0935b36cab72aa7952991
Sebastián Buitrago <sebastianbuitrago0811@gmail.com>
2019-12-21 18:51:28 -0500

    commit

--- js/service.js
+++ js/service.js
@@ -1,8 +1,8 @@
 // Hover Images 
-function mouseOverFunction(id1, id2, source) {
-    console.log(bullet, source);
-    bullet.style.color = ' #33ADB9';
-    this.src = source;
+function mouseOverFunction(bullet_func, source_func) {
+    console.log(bullet_func, source_func);
+    bullet_func.style.color = ' #33ADB9';
+    this.src = source_func;
     // document.getElementById(id1).style.color = ' #FF9300';
     // document.getElementById(id2).src = source;
 
@@ -21,17 +21,25 @@ var source = ['marketingdigital-hover.png',
               'desarrollo-hover-active.png',
               'administracion-hover-active.png',
               'consultorias-hover-active.png']
-var pathSource = "../IMAGES/mas-recuros/"
 
-for(var i=0; i<imagesId.length; i++){
-    var bullet = document.getElementById(bulletsId[i])
-    var image = document.getElementById(imagesId[i])
-    var completePath = pathSource+source[i]
-    image.addEventListener('mouseover', function() {
+var pathSource = "../IMAGES/mas-recuros/";
+var bullet = document.getElementById(bulletsId[0]);
+var image = document.getElementById(imagesId[0]);
+var completePath = pathSource+source[0];
+image.addEventListener('mouseover', function() {
         mouseOverFunction(bullet, completePath);
       });
+// for(var i=0; i<imagesId.length; i++){
+//     var bullet = document.getElementById(bulletsId[i])
+//     var image = document.getElementById(imagesId[i])
+//     var completePath = pathSource+source[i]
+//     image.addEventListener('mouseover', function() {
+//         mouseOverFunction(bullet, completePath);
+//       });
+//     console.log(image, bullet , completePath);
+    
 
-}
+// }
 // Main Container
 var container = document.querySelector(".container")
 container.style.height = String(window.innerHeight) + "px"; 
