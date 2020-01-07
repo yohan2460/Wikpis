27087f410de709dfe8b342b62cc542875ca288f5
Sebastián Buitrago <sebastianbuitrago0811@gmail.com>
2020-01-06 19:05:52 -0500

    ventana categoria

--- js/category.js
+++ js/category.js
@@ -1,28 +1,28 @@
 "use strict";
 // ===========Hover In  & Out Events ==================
-function mouseHoverIn(image, bullet, caption, radio, completePath){  // Color the caption an change image
-     if (!radio.checked){
-        bullet.style.color = ' #FF9300';
+function mouseHoverIn(image, caption, checkBox, completePath){  // Color the caption an change image
+     if (!checkBox.checked){
         image.src = completePath;
         caption.style.color = '#FFF';
      }
 };
 
-function mouseHoverOut(image, bullet, caption, radio, completePath){ // Restore Default values
-    if (!radio.checked){
-        bullet.style.color = '#33ADB9';
+function mouseHoverOut(image, caption, checkBox, completePath){ // Restore Default values
+    if (!checkBox.checked){
         image.src = completePath;
         caption.style.color = '#33ADB9';
     }
 };
 
-function radioChecked(){
-    console.log(this.value);
-    for(let i=0; i<radio.length; i++){
-        if (!radio[i].checked){
-            bullets[i].style.color = '#33ADB9';
+function checkBoxChecked(){
+
+    for(let i=0; i<checkBox.length; i++){
+        if (!checkBox[i].checked){
             images[i].src = pathSource + sourceHoverOut[i];
             images_caption[i].style.color = '#33ADB9';
+        }
+        else{
+            console.log(checkBox[i].value);
         }
     }
 }
@@ -30,33 +30,31 @@ function radioChecked(){
 
 var figures = document.querySelectorAll('.option')  
 var images = document.querySelectorAll('.image');
-var bullets = document.querySelectorAll('.mainContent__grid__list');
 var images_caption = document.querySelectorAll('.image_caption');
-var radio = document.querySelectorAll('input[type="radio"]');
+var checkBox = document.querySelectorAll('input[type="checkbox"]');
 
-var sourceHoverIn = ['marketingdigital-hover.png',
-    'desarrollo-hover.png',
-    'admin-hover.png',
-    'consultoria-hover.png'
+var sourceHoverIn = ['redessociales-hover.png',
+    'sem-hover.png',
+    'seo-hover.png',
+    'email-hover.png'
 ];
-var sourceHoverOut = ['marketingdigital.png',
-    'desarrollo.png',
-    'admin.png',
-    'consultoria.png'
+var sourceHoverOut = ['redessociales.png',
+    'sem.png',
+    'seo.png',
+    'email.png'
 ];
 var pathSource = "../IMAGES/mas-recuros/";
 
 for (let i = 0; i < figures.length; i++) {
     figures[i].addEventListener('mouseover', function() {
-        mouseHoverIn(images[i], bullets[i], images_caption[i], radio[i], pathSource + sourceHoverIn[i]);
+        mouseHoverIn(images[i], images_caption[i], checkBox[i], pathSource + sourceHoverIn[i]);
     });
     figures[i].addEventListener('mouseout', function() {
-        mouseHoverOut(images[i], bullets[i], images_caption[i], radio[i], pathSource + sourceHoverOut[i]);
+        mouseHoverOut(images[i], images_caption[i], checkBox[i], pathSource + sourceHoverOut[i]);
     });  
-    radio[i].addEventListener('click', radioChecked);
+    checkBox[i].addEventListener('click', checkBoxChecked);
 }
 //====== Init Script ======================================
 
 var container = document.querySelector(".container");
 container.style.height = String(window.innerHeight) + "px";
-console.log("hola");
